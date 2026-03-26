3c5496bd733e4eb2e12670c8a3fabfbf943dc5fa
dk <kudin.dima@gmail.com>
2026-03-26 17:29:43 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Montserrat } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'KawaiiCollect - Cute Collectible Figures',
+  description: 'Discover the cutest collectible figures for your collection. Shop our curated selection of high-quality anime, animal, and fantasy characters today.',
+  robots: {
+    "index": true,
+    "follow": true
+  },
+};
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${montserrat.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
